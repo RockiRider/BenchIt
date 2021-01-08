733fc168ed59af2e5707d993d2185529832da3a6
Tsotne Gvadzabia <tsotne24@hotmail.com>
2021-01-08 03:17:00 +0000

    mvc layout + sidebar working

---
+++ comp/getNonce.js
@@ -0,0 +1,17 @@
+
+function getNonce() {
+	let text = '';
+	const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+	for (let i = 0; i < 32; i++) {
+		text += possible.charAt(Math.floor(Math.random() * possible.length));
+	}
+	return text;
+}
+
+// @ts-ignore
+exports.getNonce = getNonce;
+
+module.exports = {
+	// @ts-ignore
+	getNonce
+}
--- extension.js
+++ extension.js
@@ -1,5 +1,6 @@
 const vscode = require('vscode');
 const handleBox = require('./comp/handleBox');
+const SideBarProvider = require('./comp/SideBarProvider');
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 
@@ -8,13 +9,15 @@ const handleBox = require('./comp/handleBox');
  */
 function activate(context) {
 
+	const sidebarProvider = new SideBarProvider.SidebarProvider(context.extensionUri);
+	context.subscriptions.push(vscode.window.registerWebviewViewProvider("benchMe_sidebar", sidebarProvider))
 
 	console.log('Congratulations, your extension "benchme" is now active!');
 
 
 	context.subscriptions.push(vscode.commands.registerCommand('benchme.addCase', function () {
 		handleBox.getInput("Tsot");
-		
+
 	}));
 }
 // @ts-ignore
