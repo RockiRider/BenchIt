e53340e12c1b05b66ff661c52bee7ee6d970e5bd
Tsotne Gvadzabia <tsotne24@hotmail.com>
2021-01-08 01:40:20 +0000

    working menu icon + custom module

--- extension.js
+++ extension.js
@@ -1,7 +1,5 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
-
+const handleBox = require('./comp/handleBox');
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 
@@ -10,21 +8,14 @@ const vscode = require('vscode');
  */
 function activate(context) {
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
+
 	console.log('Congratulations, your extension "benchme" is now active!');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with  registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('benchme.helloWorld', function () {
-		// The code you place here will be executed every time your command is executed
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from BenchMe!');
-	});
-
-	context.subscriptions.push(disposable);
+	context.subscriptions.push(vscode.commands.registerCommand('benchme.addCase', function () {
+		handleBox.getInput("Tsot");
+		
+	}));
 }
 // @ts-ignore
 exports.activate = activate;
